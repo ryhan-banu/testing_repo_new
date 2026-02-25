458d161893835f144c0019ef5be300379ffca1c9
ryhan-banu <ryhan_banu@thbs.com>
2026-02-25 12:06:56 +0530

    Fix duplicate input and print statements in test.py
    Removed duplicate input and print statements.

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ num1 =int(input("enter first number"))
 num2 =int(input("enter second number"))
 sum =num1+num2
 print("the sum is",sum)
+num1 =int(input("enter first number"))
+num2 =int(input("enter second number"))
+sum =num1+num2
+print("the sum is",sum)
